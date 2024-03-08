b72c7dbfa479e8a83a3fbf59c6abf52fe7e8f990
bervalim <bernardogvalim@gmail.com>
2024-03-08 02:09:00 -0300

    feat/contactIcon-CRUD

--- src/modules/contact-icons/contact-icons.controller.ts
+++ src/modules/contact-icons/contact-icons.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+} from '@nestjs/common';
 import { ContactIconsService } from './contact-icons.service';
 import { CreateContactIconDto } from './dto/create-contact-icon.dto';
 import { UpdateContactIconDto } from './dto/update-contact-icon.dto';
@@ -19,16 +28,20 @@ export class ContactIconsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.contactIconsService.findOne(+id);
+    return this.contactIconsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateContactIconDto: UpdateContactIconDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateContactIconDto: UpdateContactIconDto,
+  ) {
     return this.contactIconsService.update(+id, updateContactIconDto);
   }
 
+  @HttpCode(204)
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.contactIconsService.remove(+id);
+    return this.contactIconsService.remove(id);
   }
 }
--- src/modules/contact-icons/contact-icons.service.ts
+++ src/modules/contact-icons/contact-icons.service.ts
@@ -1,26 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateContactIconDto } from './dto/create-contact-icon.dto';
 import { UpdateContactIconDto } from './dto/update-contact-icon.dto';
+import { PrismaService } from 'prisma/prisma.service';
+import { ContactIcon } from './entities/contact-icon.entity';
 
 @Injectable()
 export class ContactIconsService {
-  create(createContactIconDto: CreateContactIconDto) {
-    return 'This action adds a new contactIcon';
+  constructor(private prisma: PrismaService) {}
+
+  async create(
+    createContactIconDto: CreateContactIconDto,
+  ): Promise<ContactIcon> {
+    const newContactIcon = Object.assign(
+      new ContactIcon(),
+      createContactIconDto,
+    );
+    await this.prisma.contactIcons.create({ data: { ...newContactIcon } });
+    return newContactIcon;
   }
 
-  findAll() {
-    return `This action returns all contactIcons`;
+  async findAll(): Promise<ContactIcon[]> {
+    const contactIcons = await this.prisma.contactIcons.findMany();
+    return contactIcons;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} contactIcon`;
+  async findOne(id: string): Promise<ContactIcon> {
+    const oneContactIcon = await this.prisma.contactIcons.findUnique({
+      where: { id: id },
+    });
+    if (!oneContactIcon) throw new NotFoundException('Contact Icon Not Found');
+
+    return oneContactIcon;
   }
 
   update(id: number, updateContactIconDto: UpdateContactIconDto) {
     return `This action updates a #${id} contactIcon`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} contactIcon`;
+  async remove(id: string): Promise<void> {
+    const oneContactIcon = await this.prisma.contactIcons.findUnique({
+      where: { id: id },
+    });
+    if (!oneContactIcon) throw new NotFoundException('Contact Icon Not Found');
+
+    await this.prisma.contactIcons.delete({ where: { id } });
   }
 }
