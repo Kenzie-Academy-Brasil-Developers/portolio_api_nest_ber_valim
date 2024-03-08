0aa1a9c9537c16359229da8e0de5659c1c2faca6
bervalim <bernardogvalim@gmail.com>
2024-03-07 21:13:05 -0300

    feat/Tech-DTO

--- src/modules/techs/dto/create-tech.dto.ts
+++ src/modules/techs/dto/create-tech.dto.ts
@@ -1,1 +1,7 @@
-export class CreateTechDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTechDto {
+  @IsString()
+  @IsNotEmpty()
+  techIcon: string;
+}
--- src/modules/techs/entities/tech.entity.ts
+++ src/modules/techs/entities/tech.entity.ts
@@ -1,1 +1,10 @@
-export class Tech {}
+import { randomUUID } from 'crypto';
+
+export class Tech {
+  readonly id: string;
+  techIcon: string;
+
+  constructor() {
+    this.id = randomUUID();
+  }
+}
