549301f97070455147b5d2c6e3f934987f268f90
bervalim <bernardogvalim@gmail.com>
2024-03-11 10:24:26 -0300

    feat/enable-CORS

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,21 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const config = new DocumentBuilder()
-  .setTitle('portfolio API')
-  .setDescription('Api de imagens e ícones para o meu portfólio pessoal!')
-  .setVersion('1.0')
-  .addTag('portfolio')
-  .build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
+    .setTitle('portfolio API')
+    .setDescription('Api de imagens e ícones para o meu portfólio pessoal!')
+    .setVersion('1.0')
+    .addTag('portfolio')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
